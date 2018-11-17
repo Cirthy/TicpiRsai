cf7b243647b1ddabf184cc933e04b198472be70e
Cirthy <clement.hoffmann@hotmail.fr>
2018-11-17 08:40:23 +0100

    RSA fully working

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ from prime_generator import *
 from rsa import *
 from display import *
 
-#pid = os.fork()
-#if(not pid):
-#	display_waiting(pid)
 p, q = random_prime(config.primeSize), random_prime(config.primeSize)
-#os.kill(pid, 15)
-cypher = encrypt("Coucou", p * q)
-print(str(cypher) + "\n\n___________________\n")
+cypher = encrypt(input("Enter message : "), p * q)
 plain = decrypt(cypher, p, q)
-print(plain)
+print("\n\n" + plain)
--- rsa.py
+++ rsa.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
 import config
+import binascii
 
 def encrypt(plain, n):
-	encodedPlain = ''
-	for i in (plain.encode('UTF-8')):
-		encodedPlain += str(hex(i))[2:]
-	print(str(int(encodedPlain, 16)) + "\n\n")
-	return lpowmod(int(encodedPlain, 16), config.e, n)
+	encodedPlain = int(binascii.hexlify(plain.encode('utf-8')), 16)
+	return lpowmod(encodedPlain, config.e, n)
 
 def decrypt(cipher, p, q):
 	d = modinv(config.e, (p - 1) * (q - 1))
-	return lpowmod(cipher, d, p * q)
+	encodedPlain = lpowmod(cipher, d, p * q)
+	return binascii.unhexlify(hex(encodedPlain)[2:].encode('ascii')).decode('utf-8')
 
 def ecgd(a, b):
 	x, y, u, v = 0, 1, 1, 0
